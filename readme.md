71953a02d04677a2516b0e9d052f807557ecdba6
l4br13 <t2051pz@gmail.com>
2022-11-04 13:53:38 +0700

    Fri Nov  4 01:53:38 PM WIB 2022

---
+++ readme.md
@@ -0,0 +1,16 @@
+# alpine
+alpine - linux mini rootfs wrapper script
+
+# Installation
+```bash
+alpine --install
+```
+
+# Synopsis
+
+alpine [command or options]
+
+see more with
+```bash
+alpine --help
+```
--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-this=$(dirname $(realpath $0))
-os=$(uname -o)
-if [ "$os" == "Android" ]; then
-	rm -rf $PREFIX/../alpine
-elif [ "$os" == "GNU/Linux" ]; then
-	rm -rf $this/rootfs
-	rm -rf $this/init
-fi
