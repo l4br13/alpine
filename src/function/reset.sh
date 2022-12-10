06897f94f00ab8f6992a97054ea492f30e56b466
l4br13 <t2051pz@gmail.com>
2022-12-11 00:36:06 +0700

    interactive message

--- src/function/reset.sh
+++ src/function/reset.sh
@@ -1,7 +1,9 @@
 __reset__ () {
+	printf "$(basename $0): reset\n"
 	printf "Are you sure want to reset [Y/n]? "
 	read c
 	if [ "$c" != "y" ]; then
+		printf "operation canceled by the user.\n"
 		exit 1
 	fi
 	if [ ! -z $sudo ]; then
@@ -9,5 +11,6 @@ __reset__ () {
 	else
 		rm -rf $root/*
 	fi
+	printf "done.\n"
 	return 1
 }
